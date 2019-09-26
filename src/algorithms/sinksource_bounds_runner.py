5edddc40e293e67735d4332c57f1da3981952e28
jlaw9 <jeffreynlaw@gmail.com>
2019-09-26 01:22:07 -0400

    fix minor bug

--- src/algorithms/sinksource_bounds_runner.py
+++ src/algorithms/sinksource_bounds_runner.py
@@ -181,10 +181,10 @@ def run(run_obj):
         #if len(scores_arr) < goid_scores.shape[1]:
         #    scores_arr = np.append(scores_arr, [0]*(goid_scores.shape[1] - len(scores_arr)))
         # limit the scores to the target nodes
-        if len(run_obj.target_prots) != len(scores):
-            mask = np.ones(len(scores), np.bool)
+        if len(run_obj.target_prots) != len(scores_arr):
+            mask = np.ones(len(scores_arr), np.bool)
             mask[run_obj.target_prots] = 0
-            scores[mask] = 0
+            scores_arr[mask] = 0
         goid_scores[idx] = scores_arr
         # make sure 0s are removed
         #goid_scores.eliminate_zeros()
