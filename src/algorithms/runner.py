4f61c6f867c2f1b422d1eb6506c4e81cb20f8100
Jeff Law <jlaw@nrel.gov>
2021-06-22 13:42:58 -0600

    Add RWR and LazyRW

--- src/algorithms/runner.py
+++ src/algorithms/runner.py
@@ -8,7 +8,7 @@ from scipy import sparse as sp
 from . import fastsinksource_runner as fastsinksource
 from . import sinksource_bounds_runner as ss_bounds
 from . import genemania_runner as genemania
-from . import svm_runner as svm
+from . import rwr_runner as rwr
 from . import logistic_regression_runner as logistic_regression
 
 
@@ -23,8 +23,11 @@ LibMapper = {
     'localplus': fastsinksource,
     'genemania': genemania,
     'genemaniaplus': genemania,
-    'svm': svm,
+    'rl': genemania,
+    'svm': logistic_regression,
     'logistic_regression': logistic_regression,
+    'lazy_rw': rwr,
+    'rwr': rwr,
 }
 
 
@@ -43,15 +46,15 @@ class Runner(object):
         params.pop('should_run', None)  # remove the should_run parameter
         self.params = params
         self.kwargs = kwargs
-        self.verbose = kwargs.get('verbose', False) 
-        self.forced = kwargs.get('forcealg', False) 
+        self.verbose = kwargs.get('verbose', False)
+        self.forced = kwargs.get('forcealg', False)
         # for term-based algorithms, can limit the terms for which they will be run
         self.terms_to_run = kwargs.get('terms_to_run', ann_obj.terms)
         # also can limit the nodes for which scores are stored with this
         self.target_prots = kwargs.get('target_nodes', np.arange(len(ann_obj.prots)))
 
         # track measures about each run (e.g., running time)
-        self.params_results = defaultdict(int) 
+        self.params_results = defaultdict(int)
         # store the node scores for each GO term in a sparse matrix
         # using lil matrix so 0s are automatically not stored
         self.term_scores = sp.lil_matrix(ann_obj.ann_matrix.shape, dtype=np.float)
@@ -60,13 +63,13 @@ class Runner(object):
         eval_str = get_eval_str(name, **kwargs)
         # if cross valudation is run, then the eval str will be set there, so don't include it here
         if kwargs.get('cross_validation_folds') is not None:
-            eval_str = "" 
+            eval_str = ""
         self.setupParamsStr(eval_str+net_obj.weight_str, params, name)
         default_out_pref = "%s/pred-scores%s%s" % (
             self.out_dir, self.params_str, self.kwargs.get('postfix',''))
         self.out_pref = kwargs.get('out_pref', default_out_pref)
 
-        # for the supervised classification methods, train_mat and test_mat are set during cross validation  
+        # for the supervised classification methods, train_mat and test_mat are set during cross validation 
         # leave them as None so they are ignored during prediction mode
         self.train_mat = None
         self.test_mat = None
@@ -81,7 +84,7 @@ class Runner(object):
     def run(self):
         return LibMapper[self.name].run(self)
 
-    # if the method is not in Python and was called elsewhere (e.g., R), 
+    # if the method is not in Python and was called elsewhere (e.g., R),
     # then parse the outputs of the method
     def setupOutputs(self, **kwargs):
         return LibMapper[self.name].setupOutputs(self, **kwargs)
@@ -98,7 +101,7 @@ def get_eval_str(name, **kwargs):
     # if we sampled negative examples when running this method, add that to the output string
     neg_factor = kwargs.get('sample_neg_examples_factor')
     num_reps = kwargs.get('num_reps', 1)
-    eval_str = "" 
+    eval_str = ""
     # TODO make a better way of indicating an alg needs negative examples than just having 'plus' in the name
     if 'plus' not in name and neg_factor is not None:
         # make sure its treated as an integer if it is one
@@ -115,7 +118,7 @@ def get_weight_str(dataset):
     weight_method = ""
     # if the weight method is to simply add the networks together, I did not include anything in the output file
     if dataset.get('multi_net',False) is True and \
-       dataset.get('net_settings') is not None and dataset['net_settings'].get('weight_method','add') != 'add': 
+       dataset.get('net_settings') is not None and dataset['net_settings'].get('weight_method','add') != 'add':
         weight_method = "-"+dataset['net_settings']['weight_method']
     weight_str = '%s%s' % (
         '-unw' if unweighted else '', weight_method)
@@ -129,9 +132,8 @@ def get_runner_params_str(name, params, dataset=None, weight_str="", **kwargs):
     """
     # get the weight str used when writing output files
     if dataset is not None:
-        weight_str = get_weight_str(dataset) 
-    eval_str = get_eval_str(name, **kwargs) 
+        weight_str = get_weight_str(dataset)
+    eval_str = get_eval_str(name, **kwargs)
     settings_str = eval_str + weight_str
 
     return LibMapper[name].setup_params_str(settings_str, params, name=name)
-
